d2161f4186f05c5c538f6e4dfbfd56e8e3dd8d58
Zoli1212 <43321960+Zoli1212@users.noreply.github.com>
2020-08-10 07:54:16 +0200

    my first commit

---
+++ src/test/java/com/agilintelligence/ppmtool/PpmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agilintelligence.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
